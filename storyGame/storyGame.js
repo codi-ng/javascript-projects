bce13146aca99a45f3daf1562096da99b127c8f6
codi-ng <wolfwolf28wolf@gmail.com>
2022-10-09 10:19:39 -0400

    #

--- storyGame/storyGame.js
+++ storyGame/storyGame.js
@@ -1,19 +1,21 @@
 //create a game/program that is basically a story/RPG game
 //will probably be my biggest project yet
 //test5
-
 import pkg from 'readline-sync';
 const  readlineSync  = pkg;
+
 
 import chalk from 'chalk'
 
 import { setTimeout } from 'node:timers'
 
 import { exit } from 'node:process'
+
 import { read } from 'node:fs';
 
 
-const usrHelp = "Hello, this is a help manual to help you get acquainted with how you play this story game and the different keywords, to exit the game entirely input 'fq', which stands for 'forcequit'"
+
+const usrHelp = chalk.greenBright.underline("Hello, this is a help manual to help you get acquainted with how you play this story game and the different keywords, to exit the game entirely input 'fq', which stands for 'forcequit'")
 
 console.log("")
 console.log("")
@@ -34,4 +36,4 @@ console.log(chalk.bgRed.bold("--------------------------------------------------
 console.log(" ")
 console.log(chalk.red("======================================================================== ") + chalk.whiteBright.underline.bold("Are you ready to start?") + chalk.red(" ========================================================================="))
 
-let varStartTestyN = readlineSync.question(chalk.bold("                                                                                   [Y/N] ")).toUpperCase()
+let varStartTestyN = readlineSync.keyInYN(chalk.bold("                                                                                   [Y/N] "))
