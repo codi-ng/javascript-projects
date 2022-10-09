9546eefdb5eae0db6cda99e04c88217cc65e6a20
codi-ng <wolfwolf28wolf@gmail.com>
2022-10-08 21:46:41 -0400

    #

--- storyGame/storyGame.js
+++ storyGame/storyGame.js
@@ -3,6 +3,14 @@
 //test3
 let readlineSync = require("readline-sync")
 
+import chalk from '../node_modules/chalk/source'
+
+let { setTimeout } = require("node:timers")
+
+let { exit } = require("node:process")
+
+const usrHelp = "Hello, this is a help manual to help you get acquainted with how you play this story game and the different keywords, to exit the game entirely input 'fq', which stands for 'forcequit'"
+
 console.log("")
 console.log("")
 console.log("")
@@ -20,3 +28,4 @@ console.log(" ")
 console.log("----------------------------------------------------------------------------------------------------------------------------------------------------------------")
 
 console.log("")
+console.log(chalk.blue(usrHelp));
