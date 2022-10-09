f06767ba76f8deb541b81c59ecd2858e742ed81b
codi-ng <wolfwolf28wolf@gmail.com>
2022-10-08 22:09:27 -0400

    #

--- storyGame/storyGame.js
+++ storyGame/storyGame.js
@@ -1,13 +1,21 @@
 //create a game/program that is basically a story/RPG game
 //will probably be my biggest project yet
 //test3
-let readlineSync = require("readline-sync")
+
+import pkg from 'readline-sync';
+const { readlineSync } = pkg;
+
+// let readlineSync = require("readline-sync")
 
 import chalk from 'chalk'
 
-let { setTimeout } = require("node:timers")
+import { setTimeout } from 'node:timers'
 
-let { exit } = require("node:process")
+// let { setTimeout } = require("node:timers")
+
+import { exit } from 'node:process'
+
+// let { exit } = require("node:process")
 
 const usrHelp = "Hello, this is a help manual to help you get acquainted with how you play this story game and the different keywords, to exit the game entirely input 'fq', which stands for 'forcequit'"
 
@@ -25,7 +33,7 @@ console.log("")
 console.log("========================================================================= Welcome To storyGame.js ==============================================================")
 console.log(" ")
 
-console.log("----------------------------------------------------------------------------------------------------------------------------------------------------------------")
+console.log(chalk.bgGray("----------------------------------------------------------------------------------------------------------------------------------------------------------------"))
 
 console.log("")
 console.log(chalk.blue(usrHelp));
