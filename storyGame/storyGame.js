d0e6be3fe33fe6f479d646db0dbfb130cea48eea
codi-ng <wolfwolf28wolf@gmail.com>
2022-10-09 10:48:51 -0400

    lil update

--- storyGame/storyGame.js
+++ storyGame/storyGame.js
@@ -1,6 +1,7 @@
 //create a game/program that is basically a story/RPG game
 //will probably be my biggest project yet
 //test5
+//note, when using keyinYn or something idk, remember it doesnt return the letter you chose whether y or n it returns 'true' for y and 'false' for n
 import pkg from 'readline-sync';
 const  readlineSync  = pkg;
 
@@ -15,7 +16,7 @@ import { read } from 'node:fs';
 
 
 
-const usrHelp = chalk.greenBright.underline("Hello, this is a help manual to help you get acquainted with how you play this story game and the different keywords, to exit the game entirely input 'fq', which stands for 'forcequit'")
+const usrHelp = chalk.greenBright.underline("Hello, this is a help manual to help you get acquainted with how you play this story game and the different keywords, to exit the game entirely input 'fq'\n which stands for 'forcequit'")
 
 console.log("")
 console.log("")
@@ -27,6 +28,7 @@ console.log("")
 console.log("")
 console.log("")
 
+// console.log(usrHelp);
 
 console.log(chalk.red("======================================================================== ") + chalk.bold.white.italic.underline("Welcome To storyGame.js") + chalk.red(" ========================================================================="))
 console.log("==========================================================================================================================================================================")
@@ -36,4 +38,4 @@ console.log(chalk.bgRed.bold("--------------------------------------------------
 console.log(" ")
 console.log(chalk.red("======================================================================== ") + chalk.whiteBright.underline.bold("Are you ready to start?") + chalk.red(" ========================================================================="))
 
-let varStartTestyN = console.log(readlineSync.keyInYNStrict(chalk.bgWhiteBright("                                                            ")));
+let varStartTestyN = readlineSync.keyInYNStrict(chalk.white("                                                                                  "))
