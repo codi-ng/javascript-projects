6ec3ffeaeb22ca8a2765560c14b92434a46aee2c
codi-ng <108873695+codi-ng@users.noreply.github.com>
2022-10-09 00:35:06 -0400

    #

--- storyGame/storyGame.js
+++ storyGame/storyGame.js
@@ -3,13 +3,14 @@
 //test5
 
 import pkg from 'readline-sync';
-const { readlineSync } = pkg;
+const  readlineSync  = pkg;
 
 import chalk from 'chalk'
 
 import { setTimeout } from 'node:timers'
 
 import { exit } from 'node:process'
+import { read } from 'node:fs';
 
 
 const usrHelp = "Hello, this is a help manual to help you get acquainted with how you play this story game and the different keywords, to exit the game entirely input 'fq', which stands for 'forcequit'"
@@ -25,11 +26,13 @@ console.log("")
 console.log("")
 
 
-console.log(chalk.red("========================================================================= ") + chalk.bold.white.italic.underline("Welcome To storyGame.js") + chalk.red(" =============================================================="))
-console.log(" ")
-console.log(" ")
-console.log(" ")
+console.log(chalk.red("========================================================================= ") + chalk.bold.white.italic.underline("Welcome To storyGame.js") + chalk.red(" ========================================================================="))
+console.log("===========================================================================================================================================================================")
+console.log(chalk.redBright.bgRed("|                                                                                                                                                                         |"))
+// console.log(chalk.redBright.bgRed("|                                                                                                                                                                         |"))
 
-console.log(chalk.bgRed.bold("----------------------------------------------------------------------------------------------------------------------------------------------------------------"))
+console.log(chalk.bgRed.bold("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------"))
+console.log(" ")
+console.log(chalk.red("========================================================================= ") + chalk.whiteBright.underline.bold("Are you ready to start?") + chalk.red(" ========================================================================="))
 
-console.log("")
+let varStartTestyN = readlineSync.question(chalk.bold("                                                                                   [Y/N] "))
