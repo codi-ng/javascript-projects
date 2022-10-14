4e1982fcda6d5a12b9197d983cade52ea12c67b8
codi-ng <108873695+codi-ng@users.noreply.github.com>
2022-10-13 22:13:23 -0400

    #

---
+++ askBen/askBen.js
@@ -0,0 +1,86 @@
+//do talking ben
+
+import readlineSync from 'readline-sync'
+
+import chalk from 'chalk'
+
+import process from 'node:process'
+
+import stdout from 'node:process'
+
+
+let benChoose = Math.ceil(Math.random() * 4)
+
+let startBen;
+
+let invisibleScore = 0;
+
+process.stdout.write(chalk.yellow.italic("Call ben? ")); startBen = readlineSync.keyInYNStrict("")
+
+
+switch(benChoose){
+    case 1:
+        benChoose = chalk.green.bold.underline('No')
+        break;
+    case 2:
+        benChoose = chalk.green.bold.underline('Yes')
+        break;
+    case 3:
+        benChoose = chalk.green.bold.underline('Hohoho 😂')
+        break;
+    case 4:
+        benChoose = chalk.green.bold.underline('Eugh 😒')
+        break;
+}
+
+//function startGame(){
+    let usrInput = readlineSync.question("Ask ben a question: ")
+
+
+    if( startBen === true){
+    console.log(benChoose)
+    }
+    else{
+    process.exitCode = 0
+    }
+
+    if(usrInput.includes(" ")){
+        invisibleScore++
+    }
+//}
+
+// startGame();
+
+
+// while (invisibleScore >= 1){
+
+//     let benChoose2 = Math.ceil(Math.random()* 4)
+
+//     switch(benChoose2){
+//         case 1:
+//             benChoose2 = chalk.green.bold.underline('No')
+//             break;
+//         case 2:
+//             benChoose2 = chalk.green.bold.underline('Yes')
+//             break;
+//         case 3:
+//             benChoose2 = chalk.green.bold.underline('Hohoho 😂')
+//             break;
+//         case 4:
+//             benChoose2 = chalk.green.bold.underline('Eugh 😒')
+//             break;
+//     }
+
+//     let usrInput2 = readlineSync.keyInYNStrict("Would you like to ask again?")
+
+//     startGame()
+
+//     if(invisibleScore === 5){
+//         break;
+//     }
+//     if (usrInput2 === false){
+//         // break;
+//         process.exit(0)
+//         process.exitCode = 0
+//     }
+// };
