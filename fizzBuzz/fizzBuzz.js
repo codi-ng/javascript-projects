3b828dae4eaf1eb1bf68bd61a2c04c6fd14679b5
codi-ng <wolfwolf28wolf@gmail.com>
2025-05-20 18:40:39 -0400

    Re-made fizzbuzz, it wasn't working right before.

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,6 +1,18 @@
-for(let i = 0; i <= 100; i++){
-    
-    i % 3 == 0 ? console.log("Fizz") : 
-    i % 5 == 0 ? console.log("Buzz") : 
-    i % 3 == 0 && i % 5 == 0 ? console.log("FizzBuz") : console.log(i);
+import readlineSync from 'readline-sync'
+
+let num = readlineSync.questionInt('Enter a number: ');
+
+for(let i = 1; i <= num; i++){
+	if(i % 3 == 0 && i % 5 == 0){
+		console.log('FizzBuzz');
+	}
+	else if(i % 3 == 0){
+		console.log('Fizz');
+	}
+	else if(i % 5 ==0){
+		console.log('Buzz');
+	}
+	else{
+		console.log(i);
+	}
 }
