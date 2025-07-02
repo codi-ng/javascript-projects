a8f00373b31e1709733803e2d93d5596b88f99f2
codi-ng <wolfwolf28wolf@gmail.com>
2025-07-01 21:00:50 -0400

    Made the hashmap *better*. might make it better later on

--- DSA/hashtable.js
+++ DSA/hashtable.js
@@ -1,26 +1,75 @@
-class HashMap{
-    constructor(tableSize = 16){
-        this.table = new Array(tableSize);
-        this.tableSize = tableSize;
+class HashMap {
+  constructor(tableSize) {
+    this.table = new Array(tableSize);
+    this.tableSize = tableSize;
+  }
+
+  hash(key) {
+    if (typeof key !== "string") String(key);
+    let hashCode = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode += (hashCode * 31 + key.charCodeAt(i)) % this.tableSize;
     }
 
-    hash(key){
-        let hashValue = 0;
-        for(let i = 0; i < key.length; i++){
-            hashValue = (hashValue + key.charCodeAt(i)) % this.tableSize;
+    return hashCode;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+
+    if(!this.table[index]){
+      this.table[index] = [];
+    }
+
+    for(let pair of this.table[index]){
+      if(pair[0] === key){
+        pair[1] === value;
+      }
+    }
+
+    this.table[index].push([key, value]);
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    let bucket = this.table[index];
+    if(!bucket) return undefined;
+
+    for(let pair of this.table[index]){
+      if(pair[0] === key) return pair[1]; 
+    }
+
+    return false;
+  }
+  remove(key) {
+    const index = this.hash(key);
+    const bucket = this.table[index];
+    if(!bucket) return undefined;
+
+    for(let i = 0; i < bucket.length; i++){
+      if(bucket[i][0] === key){
+        bucket.splice(i, 1);
+        return;
+      }
+    }
+
+
+  }
+
+  resize(newSize) {
+    let oldTable = this.table;
+    this.table = new HashMap(newSize);
+
+    for(let bucket of oldTable){
+      if(bucket){
+        for(let [key, value] of bucket){
+          this.set(key, value);
         }
-
-        return hashValue;
+      }
     }
 
-    set(key, value){
-        const index = this.has(key);
 
-        this.table[index] = value;
-    }
-
-    remove(key){
-        const index = this.hash(key);
-        delete this.table[index];
-    }
+  }
 }
+
