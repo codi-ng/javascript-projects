fcbb72108a7e6b3b610a7efec1ca32af44581780
codi-ng <wolfwolf28wolf@gmail.com>
2025-07-04 15:21:11 -0400

    Refactor

--- randomCat/randomCat.js
+++ randomCat/randomCat.js
@@ -4,16 +4,23 @@ const img = document.getElementById('image');
 const btn = document.getElementById('cat-btn');
 const catLoading = document.getElementById('cat-load-text');
 
-btn.addEventListener('click', async () => {
-    catLoading.textContent = 'Getting a cute cat!';
-    img.src = './cat-loading.png';
+async function getCat(){
     const response = await fetch(
         'https://api.thecatapi.com/v1/images/search?size=small&limit=1',
         { mode: 'cors' }
     );
     const data = await response.json();
 
+    return data[0].url
+}
+
+
+btn.addEventListener('click', async () => {
+    catLoading.textContent = 'Getting a cute cat!';
+    img.src = './cat-loading.png';
+    const cat = await getCat();
+
     catLoading.textContent = '';
-    img.src = data[0].url;
-    console.log(data[0].url);
+    img.src = cat;
+    console.log(cat);
 });
